89be5665aa6eafa546b51dcaa715357fad2277cc
HayaLabib <haya.moh07@gmail.com>
2024-03-26 19:54:05 +0200

    invoked createRace function
    runCountdown function called and implemented

--- src/client/assets/javascript/index.js
+++ src/client/assets/javascript/index.js
@@ -83,13 +83,16 @@ async function handleCreateRace() {
 	const track_id= store.track_id;
 	
 	// const race = TODO - invoke the API call to create the race, then save the result
+	const race= await createRace(player_id,track_id);
 
 	// TODO - update the store with the race id
 	// For the API to work properly, the race id should be race id - 1
-	store.race_id=1;
+	store.race_id= 1;
 	
 	// The race has been created, now start the countdown
 	// TODO - call the async function runCountdown
+
+	runCountdown();
 
 	// TODO - call the async function startRace
 
@@ -124,12 +127,18 @@ async function runCountdown() {
 		let timer = 3
 
 		return new Promise(resolve => {
+			const timerId= setInterval(()=>{console.log(timer)},1000)
 			// TODO - use Javascript's built in setInterval method to count down once per second
 
 			// run this DOM manipulation to decrement the countdown for the user
 			document.getElementById('big-numbers').innerHTML = --timer
 
 			// TODO - if the countdown is done, clear the interval, resolve the promise, and return
+			
+			if(timerId==0){
+				clearInterval(timerId);
+				resolve('countdown complete')
+			}
 
 		})
 	} catch(error) {
@@ -367,7 +376,7 @@ async function getRacers() {
 	// GET request to `${SERVER}/api/cars`
 }
 
-function createRace(player_id, track_id) {
+async function createRace(player_id, track_id) {
 	player_id = parseInt(player_id)
 	track_id = parseInt(track_id)
 	const body = { player_id, track_id }
@@ -402,7 +411,7 @@ async function getRace(id) {
 	// GET request to `${SERVER}/api/races/${id}`
 }
 
-function startRace(id) {
+async function startRace(id) {
 	return fetch(`${SERVER}/api/races/${id}/start`, {
 		method: 'POST',
 		...defaultFetchOpts(),
